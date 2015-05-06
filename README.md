351f186551a6bd057b498e92fd08e5ba2726537e
Nico <nborde@gmail.com>
2015-05-06 17:43:08 -0300

    combios para conectar la DB. Falta instalar mogoDB

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # turbo-avenger
 api rest mean test
+
+Ejemplo sacado desde: 
+https://carlosazaustre.es/blog/como-implementar-una-api-rest-con-mongodb-node-js-usando-express-v4/
+https://carlosazaustre.es/blog/como-crear-una-api-rest-usando-node-js/
--- app.js
+++ app.js
@@ -4,9 +4,18 @@ var express = require("express"),
     methodOverride = require("method-override");
     mongoose = require('mongoose');
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+
+//Connection to DB
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+    if(err) throw err;
+    console.log('Connected to Database');
+});
+
+var models = require('./models/tvshow')(app, mongoose);
 
 var router = express.Router();
 
